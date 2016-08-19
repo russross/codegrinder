e566383f7f8bd5b5833c9c792e9957801ed1fbb2
Russ Ross <russ@russross.com>
2016-08-19 13:23:23 -0600

    sml actions

--- codegrinder/sml.go
+++ codegrinder/sml.go
@@ -26,6 +26,20 @@ func init() {
 				Interactive: false,
 				Handler:     nannyHandler(standardMLUnittestGrade),
 			},
+			"run": &ProblemTypeAction{
+				Action:      "run",
+				Button:      "Run",
+				Message:     "Running %s‥",
+				Interactive: true,
+				Handler:     nannyHandler(standardMLRun),
+			},
+			"shell": &ProblemTypeAction{
+				Action:      "shell",
+				Button:      "Shell",
+				Message:     "Running PolyML shell‥",
+				Interactive: true,
+				Handler:     nannyHandler(standardMLShell),
+			},
 		},
 	}
 }
@@ -53,4 +67,34 @@ func standardMLUnittestGrade(n *Nanny, args, options []string, files map[string]
 
 	// run a.out and parse the output (in common with c++)
 	gTestAOutCommon(n, files, nil)
+}
+
+var standardMLRunaout = `#!/bin/bash
+set -e
+echo ';' > /tmp/semi
+cat *.sml /tmp/semi | rlwrap poly
+`
+
+func standardMLRun(n *Nanny, args, options []string, files map[string]string, stdin io.Reader) {
+	log.Printf("standard ML run")
+
+	// create an a.out file
+	if err := n.PutFiles(map[string]string{"a.out": standardMLaout}); err != nil {
+		n.ReportCard.LogAndFailf("error creating a.out: %v", err)
+		return
+	}
+
+	// make it executable
+	if err := n.ExecSimple([]string{"chmod", "755", "a.out"}, nil, false); err != nil {
+		return
+	}
+
+	// run a.out
+	n.ExecSimple([]string{"a.out"}, stdin, true)
+}
+
+func standardMLShell(n *Nanny, args, options []string, files map[string]string, stdin io.Reader) {
+	log.Printf("standard ML shell")
+
+	n.ExecSimple([]string{"rlwrap", "poly"}, stdin, true)
 }
