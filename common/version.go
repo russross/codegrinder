e017e451f8856657a98091b1fc573f568665961f
Russ Ross <russ@russross.com>
2016-08-26 11:35:08 -0600

    version bump

--- common/version.go
+++ common/version.go
@@ -7,7 +7,7 @@ type Version struct {
 }
 
 var CurrentVersion = Version{
-	Version:                 "2.0.1",
+	Version:                 "2.0.2",
 	GrindVersionRequired:    "2.0.1",
-	GrindVersionRecommended: "2.0.1",
+	GrindVersionRecommended: "2.0.2",
 }
