8013009ed155fe8109ad033f4c7ae1b6245c7131
Russ Ross <russ@russross.com>
2016-10-06 16:53:35 -0600

    run grind builds in parallel

--- all.sh
+++ all.sh
@@ -3,24 +3,28 @@
 set -e
 
 echo building codegrinder server
-go install github.com/russross/codegrinder/codegrinder
-
-echo installing codegrinder server
-sudo mv $GOPATH/bin/codegrinder /usr/local/bin/
-sudo setcap cap_net_bind_service=+ep /usr/local/bin/codegrinder
+go install github.com/russross/codegrinder/codegrinder &
 
 echo building grind for local machine
-go install github.com/russross/codegrinder/grind
+go install github.com/russross/codegrinder/grind &
+
+wait
+
+echo installing codegrinder server and grind tool
+sudo mv $GOPATH/bin/codegrinder /usr/local/bin/
+sudo setcap cap_net_bind_service=+ep /usr/local/bin/codegrinder
 sudo mv $GOPATH/bin/grind /usr/local/bin/
 
 echo building grind for linux
-GOOS=linux GOARCH=amd64 go build -o $GOPATH/src/github.com/russross/codegrinder/www/grind.linux github.com/russross/codegrinder/grind
+GOOS=linux GOARCH=amd64 go build -o $GOPATH/src/github.com/russross/codegrinder/www/grind.linux github.com/russross/codegrinder/grind &
 
 echo building grind for arm
-GOOS=linux GOARCH=arm go build -o $GOPATH/src/github.com/russross/codegrinder/www/grind.arm github.com/russross/codegrinder/grind
+GOOS=linux GOARCH=arm go build -o $GOPATH/src/github.com/russross/codegrinder/www/grind.arm github.com/russross/codegrinder/grind &
 
 echo building grind for macos
-GOOS=darwin GOARCH=amd64 go build -o $GOPATH/src/github.com/russross/codegrinder/www/grind.macos github.com/russross/codegrinder/grind
+GOOS=darwin GOARCH=amd64 go build -o $GOPATH/src/github.com/russross/codegrinder/www/grind.macos github.com/russross/codegrinder/grind &
 
 echo building grind for windows
-GOOS=windows GOARCH=amd64 go build -o $GOPATH/src/github.com/russross/codegrinder/www/grind.exe github.com/russross/codegrinder/grind
+GOOS=windows GOARCH=amd64 go build -o $GOPATH/src/github.com/russross/codegrinder/www/grind.exe github.com/russross/codegrinder/grind &
+
+wait
