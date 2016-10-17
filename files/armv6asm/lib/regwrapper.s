4d1e6c5c718cfa156beb25aec0bd7865bdf94869
Russ Ross <russ@russross.com>
2016-10-17 14:30:16 -0600

    clean up regwrapper for arm assembly

--- files/armv6asm/lib/regwrapper.s
+++ files/armv6asm/lib/regwrapper.s
@@ -1,6 +1,5 @@
-.global regwrapper, target_function, bad_register
-
-.text
+        .global regwrapper, target_function, bad_register
+        .text
 
 @ regwrapper
 @
@@ -13,120 +12,80 @@
 
 regwrapper:
         @ save variable registers, sp, and lr
-        push    {r0,ip}
-        ldr     r0, =.r4
-        stmea   r0, {r4,r5,r6,r7,r8,r9,r10,r11,r13,r14}
-        pop     {r0,ip}
+        ldr     ip, =.r4
+        stmea   ip, {r4,r5,r6,r7,r8,r9,r10,r11,sp,lr}
 
         @ call the user function
-        ldr     r14, =target_function
-        ldr     r14, [r14]
-        blx     r14
+        ldr     ip, =target_function
+        ldr     ip, [ip]
+        blx     ip
 
-        @ save r0 without using sp in case sp is broken
-        ldr     r14, =.r0
-        str     r0, [r14]
-
-        @ r0 = bad_register
-        ldr     r0, =0
+        @ ip = bad_register
+        mov     ip, #0
 
         @ check if the saved values match the current values
-.cr4:   ldr     r14, =.r4
-        ldr     r14, [r14]
-        cmp     r14, r4
-        beq     .cr5
-        ldr     r0, =4
-        ldr     r4, =.r4
-        ldr     r4, [r4]
+.cr4:   ldr     lr, =.r4
+        ldr     lr, [lr]
+        cmp     lr, r4
+        movne   ip, #4
+        movne   r4, lr
 
-.cr5:   ldr     r14, =.r5
-        ldr     r14, [r14]
-        cmp     r14, r5
-        beq     .cr6
-        ldr     r0, =5
-        ldr     r5, =.r5
-        ldr     r5, [r5]
+        ldr     lr, =.r5
+        ldr     lr, [lr]
+        cmp     lr, r5
+        movne   ip, #5
+        movne   r5, lr
 
-.cr6:   ldr     r14, =.r6
-        ldr     r14, [r14]
-        cmp     r14, r6
-        beq     .cr7
-        ldr     r0, =6
-        ldr     r6, =.r6
-        ldr     r6, [r6]
+        ldr     lr, =.r6
+        ldr     lr, [lr]
+        cmp     lr, r6
+        movne   ip, #6
+        movne   r6, lr
 
-.cr7:   ldr     r14, =.r7
-        ldr     r14, [r14]
-        cmp     r14, r7
-        beq     .cr8
-        ldr     r0, =7
-        ldr     r7, =.r7
-        ldr     r7, [r7]
+        ldr     lr, =.r7
+        ldr     lr, [lr]
+        cmp     lr, r7
+        movne   ip, #7
+        movne   r7, lr
 
-.cr8:   ldr     r14, =.r8
-        ldr     r14, [r14]
-        cmp     r14, r8
-        beq     .cr9
-        ldr     r0, =8
-        ldr     r8, =.r8
-        ldr     r8, [r8]
+        ldr     lr, =.r8
+        ldr     lr, [lr]
+        cmp     lr, r8
+        movne   ip, #8
+        movne   r8, lr
 
-.cr9:   ldr     r14, =.r9
-        ldr     r14, [r14]
-        cmp     r14, r9
-        beq     .cr10
-        ldr     r0, =9
-        ldr     r9, =.r9
-        ldr     r9, [r9]
+        ldr     lr, =.r9
+        ldr     lr, [lr]
+        cmp     lr, r9
+        movne   ip, #9
+        movne   r9, lr
 
-.cr10:  ldr     r14, =.r10
-        ldr     r14, [r14]
-        cmp     r14, r10
-        beq     .cr11
-        ldr     r0, =10
-        ldr     r10, =.r10
-        ldr     r10, [r10]
+        ldr     lr, =.r10
+        ldr     lr, [lr]
+        cmp     lr, r10
+        movne   ip, #10
+        movne   r10, lr
 
-.cr11:  ldr     r14, =.r11
-        ldr     r14, [r14]
-        cmp     r14, r11
-        beq     .cr13
-        ldr     r0, =11
-        ldr     r11, =.r11
-        ldr     r11, [r11]
+        ldr     lr, =.r11
+        ldr     lr, [lr]
+        cmp     lr, r11
+        movne   ip, #11
+        movne   r11, lr
 
-        @ expected value of sp is stored value +8
-.cr13:  ldr     r14, =.r13
-        ldr     r14, [r14]
-        add     r14, r14, #8
-        cmp     r14, r13
-        beq     .cr14
-        ldr     r0, =13
-        ldr     r13, =.r13
-        ldr     r13, [r13]
-        add     r13, r13, #8
+        ldr     lr, =.sp
+        ldr     lr, [lr]
+        cmp     lr, sp
+        movne   ip, #13
+        movne   sp, lr
 
-.cr14:  ldr     r14, =.r14
-        ldr     r14, [r14]
-        cmp     r14, r14
-        beq     .end
-        ldr     r0, =14
-        ldr     r14, =.r14
-        ldr     r14, [r14]
-
-        @ return
-.end:   ldr     r14, =bad_register
-        str     r0, [r14]
-        ldr     r0, =.r0
-        ldr     r0, [r0]
-        ldr     r14, =.r14
-        ldr     r14, [r14]
+        @ save bad_register and return
+        ldr     lr, =bad_register
+        str     ip, [lr]
+        ldr     lr, =.lr
+        ldr     lr, [lr]
         bx      lr
 
-.pool
-.data
-
-.r0:    .word   0
+        .data
 .r4:    .word   0
 .r5:    .word   0
 .r6:    .word   0
@@ -135,8 +94,8 @@ regwrapper:
 .r9:    .word   0
 .r10:   .word   0
 .r11:   .word   0
-.r13:   .word   0
-.r14:   .word   0
+.sp:    .word   0
+.lr:    .word   0
 
 target_function:
         .word 0
