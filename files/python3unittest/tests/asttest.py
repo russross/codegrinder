514ae0de113790a606e51740d1b91fcad5afab6b
Russ Ross <russ@russross.com>
2020-06-29 11:59:09 -0600

    ast test support file for python problems

---
+++ files/python3unittest/tests/asttest.py
@@ -0,0 +1,46 @@
+import ast
+import unittest
+
+class ASTTest(unittest.TestCase):
+
+    def setUp(self, filename):
+        """Stores the raw text of the student submission, the lines that were
+        printed when executing the student submission, and the AST tree of the
+        submission."""
+        self.printed_lines = []
+        f = open(filename)
+        text = f.read()
+        self.file = text
+        self.tree = ast.parse(text)
+        f.close()
+
+    def find_all(self, node_type):
+        """Returns all of the AST nodes matching the given node type. TODO:
+        list common node types here for easy access."""
+        nodes = []
+        for node in ast.walk(self.tree):
+            if isinstance(node, node_type):
+                nodes.append(node)
+        return nodes
+
+    def print_replacement(self, *text, **kwargs):
+        """Saves printed lines to a data member. Used by exec_solution, not
+        usually necessary in any specific test."""
+        self.printed_lines += text
+
+    def exec_solution(self):
+        """Executes the student submission."""
+        print = self.print_replacement
+        exec(self.file)
+
+    def debug_tree(self):
+        """Converts the AST tree for manual traversal. Not really necessary
+        with find_all."""
+        return ast.dump(self.tree)
+
+    def get_function_calls(self):
+        """Helper to find all of the function calls in the submission."""
+        names = []
+        for func in self.find_all(ast.Call):
+            names.append(func.func.id)
+        return names
