fd058be9cfd3da6bc2074ae60e58596ef655635d
Russ Ross <russ@russross.com>
2021-01-08 11:03:26 -0700

    new sqliteinout problem type

---
+++ files/sqliteinout/inout-stepall.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import glob
+import os.path
+import subprocess
+import sys
+
+cmd = ['sqlite3', '-init', '.sqliterc', 'database.db']
+
+# get the list of input files to process
+infiles = sorted(glob.glob('*.sql'))
+
+first = True
+for infile in infiles:
+    if not first:
+        print()
+    first = False
+
+    outfile = os.path.join('outputs', infile[:-len('.sql')] + '.expected')
+
+    c = ['python3', 'lib/inout-stepper.py', infile, outfile]
+    c.extend(cmd)
+
+    # run the program to get the actual output
+    print(' '.join(c))
+    status = subprocess.call(c)
+    if status != 0:
+        sys.exit(status)
