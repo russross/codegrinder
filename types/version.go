dbfe1b233be3a85bcfefd34a8737f5d4c7882692
Russ Ross <russ@russross.com>
2021-02-24 08:29:09 -0700

    bump thonny recommended version

--- types/version.go
+++ types/version.go
@@ -12,6 +12,6 @@ var CurrentVersion = Version{
 	Version:                  "2.6.0",
 	GrindVersionRequired:     "2.6.0",
 	GrindVersionRecommended:  "2.6.0",
-	ThonnyVersionRecommended: "2.5.3",
+	ThonnyVersionRecommended: "2.5.5",
 	ThonnyVersionRequired:    "2.5.3",
 }
