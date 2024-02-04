d85644b43a20f1b4800b6d66cf12e3f3191de2cd
Russ Ross <russ@russross.com>
2024-02-03 19:51:43 -0700

    dockerfile tweaks

--- all.sh
+++ all.sh
@@ -7,7 +7,6 @@ go install github.com/russross/codegrinder/server
 
 echo installing codegrinder server
 sudo mv `go env GOPATH`/bin/server /usr/local/bin/codegrinder
-sudo setcap cap_net_bind_service=+ep /usr/local/bin/codegrinder
 
 if [ -z "$CODEGRINDERROOT" ]; then
     CODEGRINDERROOT="$HOME"/codegrinder
--- build.sh
+++ build.sh
@@ -7,4 +7,3 @@ go install github.com/russross/codegrinder/server
 
 echo installing codegrinder server
 sudo mv `go env GOPATH`/bin/server /usr/local/bin/codegrinder
-#sudo setcap cap_net_bind_service=+ep /usr/local/bin/codegrinder
