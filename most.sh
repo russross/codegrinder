96f59e5c5ff1e228649a2398d66b2cbb916b981f
Russ Ross <russ@russross.com>
2025-09-22 17:37:23 -0600

    most.sh update for amd64

--- most.sh
+++ most.sh
@@ -11,4 +11,4 @@ fi
 
 echo building grind for linux amd64
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go install -tags netgo github.com/russross/codegrinder/cli
-mv `go env GOPATH`/bin/linux_amd64/cli "$CODEGRINDERROOT"/www/grind.linux_amd64
+mv `go env GOPATH`/bin/cli "$CODEGRINDERROOT"/www/grind.linux_amd64
